879913c5fb7793366abbabfa81816772bdbfb1ff
Miklós Lővinger <lovingermi@gmail.com>
2019-10-14 19:02:10 +0200

    hi

---
+++ sokszogek-mvp/model/Sokszog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sokszogek_mvp.model
+{
+    public abstract class Sokszog
+    {
+        public double oldalA { get; set; }
+        public double oldalB { get; set; }
+
+        public abstract double kerulet();
+        public abstract double terulet();
+
+        public Sokszog(double a, double b)
+        {
+            oldalA = a;
+            oldalB = b;
+        }
+    }
+}
---
+++ sokszogek-mvp/model/Teglalap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sokszogek_mvp.model
+{
+    public class Teglalap : Sokszog
+    {
+
+        public Teglalap(double A, double B):base(A,B)
+        {
+
+        } 
+
+        public override double kerulet()
+        {
+            return (oldalA + oldalB) * 2;
+        }
+
+        public override double terulet()
+        {
+            return oldalA * oldalB;
+        }
+    }
+}
